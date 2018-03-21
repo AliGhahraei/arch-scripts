87ea0b4e91dcaa6d293d0477c9e8f5cf4950f702
AliGhahraei <aligf94@gmail.com>
2018-03-20 23:43:04 -0600

    Update

--- arch-install/before-reboot.sh
+++ arch-install/before-reboot.sh
@@ -3,15 +3,15 @@
 # wpa_actiond and dialog are for wireless support. sudo, xorg and xorg-xinit are 
 # common packages that I always use and rsync and openssh are used to back up and 
 # recover my files
-echo 'installing packages'
-pacman -S --noconfirm wpa_actiond dialog sudo xorg xorg-xinit rsync openssh
-
 show(){
     GREEN='\033[0;32m'
     END='\033[0m'
 
     printf "\n${GREEN}${1}\n${END}"
 }
+
+show 'installing packages'
+pacman -S --noconfirm wpa_actiond dialog sudo xorg xorg-xinit rsync openssh
 
 show 'Adding user and setting its password'
 useradd -m -G wheel -s /bin/bash ali
@@ -25,8 +25,5 @@ mkdir '/etc/systemd/system/getty@tty1.service.d'
 TTY_FILE='/etc/systemd/system/getty@tty1.service.d/override.conf'
 touch $TTY_FILE
 echo -e '[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin ali --noclear %I $TERM' >> $TTY_FILE
-
-show 'Bringing my files back to me'
-rsync -aAXvz --progress /mnt/backup/ali /home/ali
 
 show 'DONE!'
