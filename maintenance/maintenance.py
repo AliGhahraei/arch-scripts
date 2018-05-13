27c47406e87d0f74b5d625c5cc5671654a07d951
Ali Ghahraei <aligf94@gmail.com>
2018-05-13 03:40:17 -0500

    Update

--- maintenance/maintenance.py
+++ maintenance/maintenance.py
@@ -13,7 +13,7 @@ REPOS = 'scripts', 'dotfiles'
 
 
 def task(message):
-    print(green(message))
+    print(green(message, bold=True))
 
 
 def info(message):
@@ -25,7 +25,7 @@ def warning(message):
 
 
 def error(message):
-    print(red(message))
+    print(red(message, bold=True))
     exit(1)
 
 
