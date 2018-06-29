5137c8ee9f9d20c673dfefa52fab63c1ae732b55
Ali Ghahraei <aligf94@gmail.com>
2018-06-29 09:17:37 -0500

    Linux support

--- maintenance/maintenance.py
+++ maintenance/maintenance.py
@@ -6,7 +6,7 @@ from os.path import expanduser, join
 from platform import system
 
 from crayons import green, yellow, blue
-from sh import brew, git, pip3  #pylint: disable=no-name-in-module
+from sh import git, pip3  #pylint: disable=no-name-in-module
 from sh.contrib import sudo as sh_sudo
 
 
@@ -15,28 +15,39 @@ GIT_DIR = expanduser(join('~', 'g'))
 REPOS = listdir(GIT_DIR)
 
 
-if SYSTEM == 'Darwin':
-    from sh import open as os_open  #pylint: disable=no-name-in-module
-else:
-    def os_open(*_, **__):
-        warning(f'MEGAsync not supported for {SYSTEM}')
 
 
 def main():
     if SYSTEM == 'Darwin':
-        task('Upgrading brew...')
+        from sh import open as os_open, brew  #pylint: disable=no-name-in-module
+
+        megasync = os_open.bake('-a', 'MEGAsync', _bg=True)
+
+        task('Upgrading with brew...')
         brew('update', _fg=True)
         brew('upgrade', _fg=True)
         brew('cask', 'upgrade', _fg=True)
+    elif SYSTEM == 'Linux':
+        from sh import megasync as mega, pacaur
+
+        megasync = mega.bake(_bg=True)
+
+        task('Upgrading with pacaur...')
+        pacaur('-Syu', _fg=True)
     else:
+        def megasync():
+            warning(f'MEGAsync not supported for {SYSTEM}')
+
         warning(f"Package managers for {SYSTEM} aren't supported")
+
 
     task('Checking git repos...')
     if all([tree_clean(join(GIT_DIR, repo)) for repo in REPOS]):
         info("Everything's clean!")
 
     task('Launching backup tool...')
-    os_open('-a', 'MEGAsync', _bg=True)
+    megasync()
+
     warning('Remember to update Emacs manually')
     info('Done!')
 
