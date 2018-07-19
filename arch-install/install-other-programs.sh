be18d94139d85467068b3cb5fc7675cebfc29159
Ali Ghahraei <aligf94@gmail.com>
2018-07-18 21:33:42 -0500

    Update

--- arch-install/install-other-programs.sh
+++ arch-install/install-other-programs.sh
@@ -1,1 +1,1 @@
-pacman -S feh rxvt-unicode emacs dunst libnotify qtile neovim ttf-inconsolata firefox-developer-edition alsa-utils htop gvfs evince xf86-input-synaptics
+pacman -S feh rxvt-unicode emacs dunst libnotify xmonad xmonad-contrib xmobar dmenu trayer scrot neovim ttf-inconsolata firefox-developer-edition alsa-utils htop gvfs evince xf86-input-synaptics
