441aab786698a66669750d2b5fe19fa389f9347e
Ali Ghahraei Figueroa <aligf94@gmail.com@>
2020-02-27 01:28:44 -0600

    Only update fisher on macOS

--- maintenance/trigger_maintenance.fish
+++ maintenance/trigger_maintenance.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 set_color green --bold; echo 'Upgrading fish shell...'; set_color normal
-fisher self-update
+if [ (uname) != "Linux" ]
+    fisher self-update
+end
 fisher
 nix_maintenance
